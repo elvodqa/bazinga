82af1431464b13f9a1a96cddfab36a73665f4bf7
elvodqa <emircengiz21@gmail.com>
2023-06-13 22:42:41 +0300

    Start parser

--- source/app.d
+++ source/app.d
@@ -1,4 +1,7 @@
 import raylib;
+version = RAYGUI_IMPLEMENTATION;
+import raygui;
+
 import std.stdio;
 import core.stdc.string : strlen;
 import std.conv : to;
@@ -10,6 +13,7 @@ import std.file;
 string window_title;
 string game_location;
 Game game;
+Font ui_font;
 
 
 int main(string[] args)
@@ -21,10 +25,26 @@ int main(string[] args)
                 write(">>> ");
                 auto test_string = readln();
                 CommandParser parser = new CommandParser(test_string);
-                writeln("Tokens:");
-                foreach (token; parser.tokens) {
-                    writeln(token);
+
+                foreach (command; parser.commands) {
+                    writeln(command);
                 }
+            }
+        }
+        else if (args[1] == "parser") {
+            import bazinga.parser;
+            auto test = `
+            talk Emir "Hello world!"
+            set_actor_sprite Emir happy
+            add_actor Emir sad
+            remove_actor Emir
+            set_scene School
+            set_window_title "AAA"
+            `;
+
+            CommandParser parser = new CommandParser(test);
+            foreach (command; parser.commands) {
+                writeln(command);
             }
         }
         else {
@@ -68,22 +88,74 @@ int main(string[] args)
     return 0;
 }
 
+enum GameState {
+    MainMenu,
+    Pause,
+    Playing,
+}
+
+GameState game_state;
+bool is_settings_open = false;
+float volume = 50.0f;
+float text_speed = 5.0f;
+
 void run_game() {
     validateRaylibBinding();
     SetConfigFlags(ConfigFlags.FLAG_VSYNC_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
     InitWindow(1280, 720, "Bazinga");
-    
     SetWindowTitle(toStringz(game.title));
-
     SetTargetFPS(60);
 
+    ui_font = LoadFontEx("resources/fonts/Aller_Rg.ttf", 20, cast(int*)0, 0);
+    GuiSetFont(ui_font);
+    
+
+    game_state = GameState.MainMenu;
+    
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(Colors.BLACK);
-        DrawText("Game", 10, 10, 20, Colors.WHITE);
-        DrawText("Bazinga - v0.0.1", 10, 100, 20, Colors.WHITE);
-        EndDrawing();
+        switch (game_state) {
+            case GameState.MainMenu:
+                BeginDrawing();
+                    ClearBackground(Colors.BLACK);
+                    //DrawTextEx(ui_font, toStringz(game.title), Vector2(10, 10), 20, 0, Colors.WHITE);
+
+                    // Draw Play, Load, Options, Quit buttons at the bottom left of the screen. Use window size to calculate.
+                    if (GuiButton(Rectangle(50, GetScreenHeight() - 150, 200, 40), "Quit")) {
+                        CloseWindow();
+                    }
+                    if (GuiButton(Rectangle(50, GetScreenHeight() - 200, 200, 40), "Settings")) {
+                        is_settings_open = !is_settings_open;
+                    }
+                    GuiButton(Rectangle(50, GetScreenHeight() - 250, 200, 40), "Load");
+                    GuiButton(Rectangle(50, GetScreenHeight() - 300, 200, 40), "Play");
+                    
+
+                    if (is_settings_open) {
+                        // Gui WindowBox at the center 600x400
+                        if (GuiWindowBox(Rectangle(GetScreenWidth() / 2 - 250, GetScreenHeight() / 2 - 300, 500, 600), "Settings")) {
+                            is_settings_open = false;
+                        }
+                        // Gui Slider for volume
+                        GuiLabel(Rectangle(GetScreenWidth() / 2 - 150, GetScreenHeight() / 2 - 265, 100, 10), "Volume");
+                        volume = GuiSlider(Rectangle(GetScreenWidth() / 2 - 220, GetScreenHeight() / 2 - 250, 200, 40), "", toStringz(to!string(volume)), volume, 0, 100);
+
+                        // Gui Slider for Text speed
+                        GuiLabel(Rectangle(GetScreenWidth() / 2 - 160, GetScreenHeight() / 2 - 165, 100, 10), "Text Speed");
+                        text_speed = GuiSlider(Rectangle(GetScreenWidth() / 2 - 220, GetScreenHeight() / 2 - 150, 200, 40), "", toStringz(to!string(text_speed)), text_speed, 1, 10);
+                    }
+                    
+                EndDrawing();
+                break;
+            case GameState.Playing:
+                
+                break;
+            case GameState.Pause:
+                
+                break;
+            default:
+            break;
+        }
     }
     CloseWindow();
 }
@@ -94,14 +166,16 @@ void no_game() {
     SetConfigFlags(ConfigFlags.FLAG_VSYNC_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
     InitWindow(1280, 720, "Bazinga - No Game :/");
 
+    ui_font = LoadFontEx("resources/fonts/Aller_Rg.ttf", 20, cast(int*)0, 0);
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(Colors.BLACK);
-        DrawText("No game :(", 10, 10, 20, Colors.WHITE);
-        DrawText("You may need to spesifiy the project folder: 'bazinga ./my_game'", 10, 38, 20, Colors.WHITE);
-        DrawText("Bazinga - v0.0.1", 10, 100, 20, Colors.WHITE);
+        DrawTextEx(ui_font, "No game specified!", Vector2(10, 10), 20, 0, Colors.WHITE);
+        DrawTextEx(ui_font, "Please specify a game to run.", Vector2(10, 50), 20, 0, Colors.WHITE);
+        DrawTextEx(ui_font, "Usage: bazinga <game>", Vector2(10, 90), 20, 0, Colors.WHITE);
         EndDrawing();
     }
     CloseWindow();
--- source/parser.d
+++ source/parser.d
@@ -1,39 +1,67 @@
 module bazinga.parser;
 import std.ascii;
 
-class Command {}
+class Command {
+}
 
 class TalkCommand : Command {
     string text;
     string speaker;
+    override
+    string toString() {
+        return "TalkCommand(" ~ this.text ~ ", " ~ this.speaker ~ ")";
+    }
 }
 
 class MoveCommand : Command {
     string target;
     string destination;
+    override
+    string toString() {
+        return "MoveCommand(" ~ this.target ~ ", " ~ this.destination ~ ")";
+    }
 }
 
 class SetActorSpriteCommand : Command {
     string actor;
     string sprite;
+    override
+    string toString() {
+        return "SetActorSpriteCommand(" ~ this.actor ~ ", " ~ this.sprite ~ ")";
+    }
 }
 
 class AddActorCommand : Command {
     string actor;
     string sprite;
-    string location;
+    override
+    string toString() {
+        return "AddActorCommand(" ~ this.actor ~ ", " ~ this.sprite ~ ")";
+    }
 }
 
 class RemoveActorCommand : Command {
     string actor;
+    override
+    string toString() {
+        return "RemoveActorCommand(" ~ this.actor ~ ")";
+    }
 }
 
 class SetSceneCommand : Command {
     string scene;
+    override
+    string toString() {
+        return "SetSceneCommand(" ~ this.scene ~ ")";
+    }
 }
 
 class SetWindowTitle : Command {
     string title;
+    override
+    string toString() {
+        return "SetWindowTitle(" ~ this.title ~ ")";
+    }
 }
 
 enum TokenType {
@@ -48,12 +76,23 @@ enum TokenType {
     Assign,
 }
 
+string[] command_types = [
+    "talk",
+    "move",
+    "set_actor_sprite",
+    "add_actor",
+    "remove_actor",
+    "set_scene",
+    "set_window_title",
+];
+
 struct Token {
     string value;
     TokenType type;
     int line;
     int column;
 }
+
 
 class CommandParser {
     Command[] commands;
@@ -67,11 +106,83 @@ class CommandParser {
         this.line = 1;
         this.column = 1;
         this.tokens = this.getTokens(source);
+        this.commands = this.ParseCommands();
     }
 
-    void ParseNextCommand() {
+    Command[] ParseCommands() {
+        Command[] commands_list;
+        import std.algorithm: canFind;
+        import std.array: split;
+        import std.string: strip;
+        Token[][] lines = this.tokens.split!(a => a.type == TokenType.NewLine);
+        foreach (Token[] line; lines) {
+            if (line.length == 0) {
+                continue;
+            }
+            if (line[0].type == TokenType.Identifier) {
+                string command = line[0].value;
+                if (command_types.canFind(command)) {
+                    if (command == "talk") {
+                        import std.stdio;
+                        TalkCommand talk_command = new TalkCommand();
+                        talk_command.speaker = line[1].value;
+                        talk_command.text = line[2].value;
+                        writeln(talk_command.toString());
+                        commands_list ~= talk_command;
+                    } else if (command == "move") {
+                        MoveCommand move_command = new MoveCommand();
+                        move_command.target = line[1].value;
+                        move_command.destination = line[2].value;
+                        commands_list ~= move_command;
+                    } else if (command == "set_actor_sprite") {
+                        SetActorSpriteCommand set_actor_sprite_command = new SetActorSpriteCommand();
+                        set_actor_sprite_command.actor = line[1].value;
+                        set_actor_sprite_command.sprite = line[2].value;
+                        commands_list ~= set_actor_sprite_command;
+                    } else if (command == "add_actor") {
+                        AddActorCommand add_actor_command = new AddActorCommand();
+                        add_actor_command.actor = line[1].value;
+                        add_actor_command.sprite = line[2].value;
+                        commands_list ~= add_actor_command;
+                    } else if (command == "remove_actor") {
+                        RemoveActorCommand remove_actor_command = new RemoveActorCommand();
+                        remove_actor_command.actor = line[1].value;
+                        commands_list ~= remove_actor_command;
+                    } else if (command == "set_scene") {
+                        SetSceneCommand set_scene_command = new SetSceneCommand();
+                        set_scene_command.scene = line[1].value;
+                        commands_list ~= set_scene_command;
+                    } else if (command == "set_window_title") {
+                        SetWindowTitle set_window_title_command = new SetWindowTitle();
+                        set_window_title_command.title = line[1].value;
+                        commands_list ~= set_window_title_command;
+                    }
+                }
+                else {
+                    // Order: 'Indetifier StringLiteral' is talk command
+                    if (line.length == 2 && line[1].type == TokenType.StringLiteral) {
+                        TalkCommand talk_command = new TalkCommand();
+                        talk_command.speaker = line[0].value;
+                        talk_command.text = line[1].value;
+                        commands_list ~= talk_command;
+                    } else {
+                        throw new Exception("Unknown command: " ~ command);
+                    }
+
+                    throw new Exception("Unknown command: " ~ command);
+                }
+            }
+        }
+
         
+
+        return commands;
     }
+
+    
+
+
+
 
     Token[] getTokens(string source) {
         Token[] tokens_list;
@@ -79,9 +190,14 @@ class CommandParser {
 
         while (index < source.length) {
             char c = source[index];
+            if (c == ' ') {
+                index++;
+                continue;
+            }
+
             if (c == '"') {
                 tokens_list ~= this.getStringLiteral(source, index);
-            } else if (c == ' ' || c == '\t') {
+            } else if (c == '\t') {
                 tokens_list ~= this.getWhiteSpace(source, index);
             } else if (c == '\n') {
                 tokens_list ~= this.getNewLine(source, index);
@@ -129,7 +245,7 @@ class CommandParser {
     Token getWhiteSpace(string source, int index) {
         int start = index;
         index++;
-        while (index < source.length && (source[index] == ' ' || source[index] == '\t')) {
+        while (index < source.length && source[index] == '\t') {
             index++;
         }
         return Token(source[start..index], TokenType.WhiteSpace, this.line, this.column);
@@ -165,7 +281,7 @@ class CommandParser {
     Token getIdentifier(string source, int index) {
         int start = index;
         index++;
-        while (index < source.length && isAlpha(source[index])) {
+        while (index < source.length && isAlpha(source[index]) || isDigit(source[index]) || source[index] == '_') {
             index++;
         }
         return Token(source[start..index], TokenType.Identifier, this.line, this.column);
